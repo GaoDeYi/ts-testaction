271b1a614a0a6d528aecd5b8836ba686febc90b9
Dennis Goetz <53948323+GaoDeYi@users.noreply.github.com>
2023-07-08 14:43:39 +0200

    Update action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,6 +3,7 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
+import { GitHub } from '../node_modules/@actions/github/lib/utils'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -19,11 +20,16 @@ test('wait 500 ms', async () => {
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+    process.env['INPUT_REPOSITORY-NAME'] = 'ATMOxTest'
+    process.env['INPUT_DESTINATION-ROOT'] = 'F:\\GitHubTest'
+    process.env['INPUT_OBJECT-TYPE'] = 'Objects'
+
+    process.env['GITHUB_PATH'] = 'F:\\GitHubTest\\xyu'
+
+    const np = process.execPath
+    const ip = path.join(__dirname, '..', 'lib', 'main.js')
+    const options: cp.ExecFileSyncOptions = {
+      env: process.env
+    }
+    console.log(cp.execFileSync(np, [ip], options).toString())
 })
